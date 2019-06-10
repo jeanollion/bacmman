c16e0ed144ba03c542aaae71e4b3702366143d95
jollion <jean.ollion@polytechnique.org>
2019-06-10 15:38:35 +0200

    initial commit

--- bacmman-ij2/src/main/java/bacmman/ui/image_interaction/IJ2ImageWindowManager.java
+++
@@ -1,7 +0,0 @@
-package bacmman.ui.image_interaction;
-
-import bacmman.ui.gui.image_interaction.ImageWindowManager;
-import net.imagej.display.ImageDisplay;
-
-public class IJ2ImageWindowManager {
-}
