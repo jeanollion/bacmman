ba8c6a06f12768767ce347645e783563acc1c126
jollion <jean.ollion@polytechnique.org>
2019-09-08 08:08:31 +0200

    fileChooser has relative path option

--- bacmman-core/src/main/java/bacmman/configuration/experiment/Experiment.java
+++ bacmman-core/src/main/java/bacmman/configuration/experiment/Experiment.java
@@ -84,8 +84,8 @@ public class Experiment extends ContainerParameterImpl<Experiment> {
     SimpleListParameter<Position> positions= new SimpleListParameter<>("Pre-Processing for all Positions", -1 , Position.class).setAllowMoveChildren(false).setHint("Positions of the dataset. Pre-processing is defined for each position. Right-click menu allows to overwrite pre-processing to other position.<br />Element that appear in blue differ from the template");
     PreProcessingChain template = new PreProcessingChain("Pre-Processing template", true).setHint("List of pre-processing operations that will be set by default to positions at import. <br />For each position those operations can be edited (either from the <em>Positions</em> branch in the <em>Configuration tab</em> or from the <em>Configuration Test</em> tab)");
     
-    protected FileChooser imagePath = new FileChooser("Output Image Path", FileChooserOption.DIRECTORIES_ONLY).setHint("Directory where preprocessed images will be stored");
-    protected FileChooser outputPath = new FileChooser("Output Path", FileChooserOption.DIRECTORIES_ONLY).setHint("Directory where segmentation & lineage results will be stored");
+    protected FileChooser imagePath = new FileChooser("Output Image Path", FileChooserOption.DIRECTORIES_ONLY, false).setHint("Directory where preprocessed images will be stored");
+    protected FileChooser outputPath = new FileChooser("Output Path", FileChooserOption.DIRECTORIES_ONLY, false).setHint("Directory where segmentation & lineage results will be stored");
     ChoiceParameter importMethod = new ChoiceParameter("Import Method", IMPORT_METHOD.getChoices(), null, false);
     TextParameter positionSeparator = new TextParameter("Position Separator", "xy", true).setHint("character sequence located just before the position index in all image files");
     TextParameter frameSeparator = new TextParameter("Frame Separator", "t", true).setHint("character sequence located just before the frame number in all image files");
@@ -107,15 +107,16 @@ public class Experiment extends ContainerParameterImpl<Experiment> {
     @Override
     public JSONObject toJSONEntry() {
         JSONObject res= new JSONObject();
-        //res.put("imagePath", imagePath.toJSONEntry());
-        //res.put("outputPath", outputPath.toJSONEntry());
         if (path==null && getOutputDirectory()!=null) path = Paths.get(getOutputDirectory()).getParent();
-        if (path!=null) {
+        res.put("imagePath", imagePath.toJSONEntry());
+        res.put("outputPath", outputPath.toJSONEntry());
+
+        /*if (path!=null) {
             String iPath = getOutputImageDirectory();
             if (iPath != null) res.put("imagePath", path.relativize(Paths.get(iPath)).toString());
             String oPath = getOutputDirectory();
             if (oPath != null) res.put("outputPath", path.relativize(Paths.get(oPath)).toString());
-        }
+        }*/
         res.put("channelImages", channelImages.toJSONEntry());
         res.put("channelImagesDuplicated", channelImagesDuplicated.toJSONEntry());
         res.put("structures", structures.toJSONEntry());
@@ -132,11 +133,15 @@ public class Experiment extends ContainerParameterImpl<Experiment> {
     public void initFromJSONEntry(Object jsonEntry) {
         if (jsonEntry==null) throw new IllegalArgumentException("Cannot init xp with null content!");
         JSONObject jsonO = (JSONObject)jsonEntry;
+        /*if (jsonO.get("imagePath") instanceof JSONArray) {
+            imagePath.initFromJSONEntry(jsonO.get("imagePath"));
+        } else if (jsonO.containsKey("imagePath"))  imagePath.setSelectedFilePath(path.resolve(Paths.get(jsonO.get("imagePath").toString())).normalize().toFile().getAbsolutePath());
+        if (jsonO.get("outputPath") instanceof JSONArray) {
+            outputPath.initFromJSONEntry(jsonO.get("outputPath"));
+        } else  if (jsonO.containsKey("outputPath")) outputPath.setSelectedFilePath(path.resolve(Paths.get(jsonO.get("outputPath").toString())).normalize().toFile().getAbsolutePath());
+        */
         if (jsonO.get("imagePath") instanceof JSONArray) imagePath.initFromJSONEntry(jsonO.get("imagePath"));
-        else if (jsonO.containsKey("imagePath"))  imagePath.setSelectedFilePath(path.resolve(Paths.get(jsonO.get("imagePath").toString())).normalize().toFile().getAbsolutePath());
         if (jsonO.get("outputPath") instanceof JSONArray) outputPath.initFromJSONEntry(jsonO.get("outputPath"));
-        else  if (jsonO.containsKey("outputPath")) outputPath.setSelectedFilePath(path.resolve(Paths.get(jsonO.get("outputPath").toString())).normalize().toFile().getAbsolutePath());
-
         channelImages.initFromJSONEntry(jsonO.get("channelImages"));
         if (jsonO.containsKey("channelImagesDuplicated")) channelImagesDuplicated.initFromJSONEntry(jsonO.get("channelImagesDuplicated"));
         structures.initFromJSONEntry(jsonO.get("structures"));
