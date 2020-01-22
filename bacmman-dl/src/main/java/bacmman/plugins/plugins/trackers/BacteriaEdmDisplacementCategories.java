9890d70d192b288a7cf72886c8437a65b907d67d
jean.ollion <jean.ollion@polytechnique.org>
2020-01-22 09:24:06 +0100

    removed bacteria EDM displacement. version with categories accept no categories

--- bacmman-dl/src/main/java/bacmman/plugins/plugins/trackers/BacteriaEdmDisplacementCategories.java
+++ bacmman-dl/src/main/java/bacmman/plugins/plugins/trackers/BacteriaEdmDisplacementCategories.java
@@ -5,6 +5,7 @@ import bacmman.data_structure.*;
 import bacmman.image.*;
 import bacmman.measurement.BasicMeasurements;
 import bacmman.plugins.*;
+import bacmman.plugins.plugins.scalers.MinMaxScaler;
 import bacmman.plugins.plugins.segmenters.BacteriaEDM;
 import bacmman.processing.ImageOperations;
 import bacmman.processing.ResizeUtils;
@@ -18,8 +19,6 @@ import java.util.function.Predicate;
 import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-import static bacmman.plugins.plugins.trackers.BacteriaEdmDisplacement.*;
 
 public class BacteriaEdmDisplacementCategories implements TrackerSegmenter, TestableProcessingPlugin {
     PluginParameter<Segmenter> edmSegmenter = new PluginParameter<>("Segmenter from EDM", Segmenter.class, new BacteriaEDM(), false).setEmphasized(true);
@@ -35,7 +34,7 @@ public class BacteriaEdmDisplacementCategories implements TrackerSegmenter, Test
     @Override
     public void segmentAndTrack(int objectClassIdx, List<SegmentedObject> parentTrack, TrackPreFilterSequence trackPreFilters, PostFilterSequence postFilters, SegmentedObjectFactory factory, TrackLinkEditor editor) {
         Map<SegmentedObject, Image>[] edm_div_dy_np = predict(objectClassIdx, parentTrack, trackPreFilters);
-        if (stores!=null) edm_div_dy_np[1].forEach((o, im) -> stores.get(o).addIntermediateImage("divMap", im));
+        if (stores!=null && edm_div_dy_np[1]!=null) edm_div_dy_np[1].forEach((o, im) -> stores.get(o).addIntermediateImage("divMap", im));
         segment(objectClassIdx, parentTrack, edm_div_dy_np[0], edm_div_dy_np[2], postFilters, factory);
         track(objectClassIdx, parentTrack ,edm_div_dy_np[2], edm_div_dy_np[3], editor);
     }
@@ -56,11 +55,12 @@ public class BacteriaEdmDisplacementCategories implements TrackerSegmenter, Test
         logger.info("input resampled in {}ms", t3-t2);
         Image[][] input = getInputs(resampledImages.key, true, next);
         Image[][][] predictions =  engine.process(input); // order: output# [dy / cat (cat_next) / edm] / batch / channel
+        boolean categories = predictions.length>2;
         int channelEdmCur = predictions[predictions.length-1][0].length==1 ? 0 : 1;
         Image[] dy = ResizeUtils.getChannel(predictions[0], 0);
         Image[] edm = ResizeUtils.getChannel(predictions[predictions.length-1], channelEdmCur);
-        Image[] divMap = stores==null ? null : ResizeUtils.getChannel(predictions[1], 2);
-        Image[] noPrevMap = ResizeUtils.getChannel(predictions[1], 3);
+        Image[] divMap = stores==null||!categories ? null : ResizeUtils.getChannel(predictions[1], 2);
+        Image[] noPrevMap = categories ? ResizeUtils.getChannel(predictions[1], 3) : null;
         boolean[] noPrevParent = new boolean[parentTrack.size()];
         noPrevParent[0] = true;
         for (int i = 1; i<noPrevParent.length; ++i) if (parentTrack.get(i-1).getFrame()<parentTrack.get(i).getFrame()-1) noPrevParent[i]=true;
@@ -85,7 +85,7 @@ public class BacteriaEdmDisplacementCategories implements TrackerSegmenter, Test
                     };
                     edm = average3.apply(new Image[][]{ResizeUtils.getChannel(predictions[predictions.length - 1], 0), edm, ResizeUtils.getChannel(predictions[predictions.length - 1], 2)});
                 }
-                // avergae on dy
+                // average on dy
                 Function<Image[][], Image[]> average2 = cn -> {
                     Image[] cur = cn[1];
                     Image[] next = cn[2];
@@ -119,7 +119,7 @@ public class BacteriaEdmDisplacementCategories implements TrackerSegmenter, Test
         Image[] edm_res = ResizeUtils.resample(edm, edm, false, resampledImages.value); // should segmentation be performed before resampling so that edm values correspond to actual distances?
         Image[] dy_res = ResizeUtils.resample(dy, dy, true, resampledImages.value);
         Image[] divMap_res = divMap==null ? null : ResizeUtils.resample(divMap, divMap, false, resampledImages.value);
-        Image[] noPrevMap_res = ResizeUtils.resample(noPrevMap, noPrevMap, true, resampledImages.value);
+        Image[] noPrevMap_res = noPrevMap==null ? null : ResizeUtils.resample(noPrevMap, noPrevMap, true, resampledImages.value);
         for (int idx = 0;idx<parentTrack.size(); ++idx) {
             edm_res[idx].setCalibration(parentTrack.get(idx).getMaskProperties());
             edm_res[idx].translate(parentTrack.get(idx).getMaskProperties());
@@ -129,14 +129,16 @@ public class BacteriaEdmDisplacementCategories implements TrackerSegmenter, Test
                 divMap_res[idx].setCalibration(parentTrack.get(idx).getMaskProperties());
                 divMap_res[idx].translate(parentTrack.get(idx).getMaskProperties());
             }
-            noPrevMap_res[idx].setCalibration(parentTrack.get(idx).getMaskProperties());
-            noPrevMap_res[idx].translate(parentTrack.get(idx).getMaskProperties());
+            if (noPrevMap_res!=null) {
+                noPrevMap_res[idx].setCalibration(parentTrack.get(idx).getMaskProperties());
+                noPrevMap_res[idx].translate(parentTrack.get(idx).getMaskProperties());
+            }
         }
         long t5= System.currentTimeMillis();
         logger.info("predicitons resampled in {}ms", t5-t4);
         Map<SegmentedObject, Image> edmM = IntStream.range(0, parentTrack.size()).mapToObj(i->i).collect(Collectors.toMap(i -> parentTrack.get(i), i -> edm_res[i]));
         Map<SegmentedObject, Image> divM = divMap_res==null ? null : IntStream.range(0, parentTrack.size()).mapToObj(i->i).collect(Collectors.toMap(i -> parentTrack.get(i), i -> divMap_res[i]));
-        Map<SegmentedObject, Image> npM = IntStream.range(0, parentTrack.size()).mapToObj(i->i).collect(Collectors.toMap(i -> parentTrack.get(i), i -> noPrevMap_res[i]));
+        Map<SegmentedObject, Image> npM = noPrevMap_res ==null ? null : IntStream.range(0, parentTrack.size()).mapToObj(i->i).collect(Collectors.toMap(i -> parentTrack.get(i), i -> noPrevMap_res[i]));
         Map<SegmentedObject, Image> dyM = IntStream.range(0, parentTrack.size()).mapToObj(i->i).collect(Collectors.toMap(i -> parentTrack.get(i), i -> dy_res[i]));
         return new Map[]{edmM, divM, dyM, npM};
     }
@@ -162,13 +164,13 @@ public class BacteriaEdmDisplacementCategories implements TrackerSegmenter, Test
     }
     public void track(int objectClassIdx, List<SegmentedObject> parentTrack, Map<SegmentedObject, Image> dy, Map<SegmentedObject, Image> noPrev, TrackLinkEditor editor) {
         if (stores!=null) dy.forEach((o, im) -> stores.get(o).addIntermediateImage("dy", im));
-        if (stores!=null) noPrev.forEach((o, im) -> stores.get(o).addIntermediateImage("noPrevMap", im));
+        if (stores!=null && noPrev!=null) noPrev.forEach((o, im) -> stores.get(o).addIntermediateImage("noPrevMap", im));
 
         Map<Region, Double> displacementMap = parentTrack.stream().flatMap(p->p.getChildren(objectClassIdx)).parallel().collect(Collectors.toMap(
                 o->o.getRegion(),
                 o-> BasicMeasurements.getQuantileValue(o.getRegion(), dy.get(o.getParent()), 0.5)[0] * o.getParent().getBounds().sizeY() / 256d // / 256d factor is due to rescaling: dy is computed in pixels in the 32x256 image.
         ));
-        Map<SegmentedObject, BacteriaEdmDisplacement.TrackingObject> objectSpotMap = parentTrack.stream().flatMap(p->p.getChildren(objectClassIdx)).parallel().collect(Collectors.toMap(o->o, o->new TrackingObject(o.getRegion(), o.getParent().getBounds(), o.getFrame(), displacementMap.get(o.getRegion()))));
+        Map<SegmentedObject, TrackingObject> objectSpotMap = parentTrack.stream().flatMap(p->p.getChildren(objectClassIdx)).parallel().collect(Collectors.toMap(o->o, o->new TrackingObject(o.getRegion(), o.getParent().getBounds(), o.getFrame(), displacementMap.get(o.getRegion()))));
 
         Map<Integer, List<SegmentedObject>> objectsF = SegmentedObjectUtils.getChildrenByFrame(parentTrack, objectClassIdx);
         removeCrossingLinks(objectsF, objectSpotMap);
@@ -183,30 +185,31 @@ public class BacteriaEdmDisplacementCategories implements TrackerSegmenter, Test
             if (objectsPrev==null || objectsPrev.isEmpty()) continue;
             Map<SegmentedObject, SegmentedObject> nextToPrevMap = Utils.toMapWithNullValues(objects.stream(), o->o, o->getClosest(o, objectsPrev, objectSpotMap), true);
             // take into account noPrev : remove link with previous cell if object is detected as noPrev and there is another cell linked to the previous cell
-            Image np = noPrev.get(objects.get(0).getParent());
-            Map<SegmentedObject, Double> noPrevO = objects.stream()
-                    .filter(o->nextToPrevMap.get(o)!=null)
-                    .filter(o->Math.abs(objectSpotMap.get(o).dy)<1) // only when no displacement is computed
-                    .collect(Collectors.toMap(o->o, o -> BasicMeasurements.getMeanValue(o.getRegion(), np)));
-            noPrevO.entrySet().removeIf(e->e.getValue()<0.5);
-            noPrevO.forEach((o, npV) -> {
-                SegmentedObject prev = nextToPrevMap.get(o);
-                if (prev!=null) {
-                    for (Map.Entry<SegmentedObject, Double> e : noPrevO.entrySet()) {
-                        if (e.getKey().equals(o)) continue;
-                        if (!prev.equals(nextToPrevMap.get(e.getKey()))) continue;
-                        if (npV>e.getValue()) {
-                            nextToPrevMap.put(o, null);
-                            logger.debug("object: {} has no prev: (was: {}) p={}", o, prev, npV);
-                            break;
-                        } else {
-                            nextToPrevMap.put(e.getKey(), null);
-                            logger.debug("object: {} has no prev: (was: {}) p={}", e.getKey(), prev, e.getValue());
+            if (noPrev!=null) {
+                Image np = noPrev.get(objects.get(0).getParent());
+                Map<SegmentedObject, Double> noPrevO = objects.stream()
+                        .filter(o -> nextToPrevMap.get(o) != null)
+                        .filter(o -> Math.abs(objectSpotMap.get(o).dy) < 1) // only when no displacement is computed
+                        .collect(Collectors.toMap(o -> o, o -> BasicMeasurements.getMeanValue(o.getRegion(), np)));
+                noPrevO.entrySet().removeIf(e -> e.getValue() < 0.5);
+                noPrevO.forEach((o, npV) -> {
+                    SegmentedObject prev = nextToPrevMap.get(o);
+                    if (prev != null) {
+                        for (Map.Entry<SegmentedObject, Double> e : noPrevO.entrySet()) {
+                            if (e.getKey().equals(o)) continue;
+                            if (!prev.equals(nextToPrevMap.get(e.getKey()))) continue;
+                            if (npV > e.getValue()) {
+                                nextToPrevMap.put(o, null);
+                                logger.debug("object: {} has no prev: (was: {}) p={}", o, prev, npV);
+                                break;
+                            } else {
+                                nextToPrevMap.put(e.getKey(), null);
+                                logger.debug("object: {} has no prev: (was: {}) p={}", e.getKey(), prev, e.getValue());
+                            }
                         }
                     }
-                }
-            });
-
+                });
+            }
             Map<SegmentedObject, Integer> nextCount = new HashMapGetCreate.HashMapGetCreateRedirected<>(o->0);
             nextToPrevMap.values().forEach(o -> {if (o!=null) nextCount.replace(o, nextCount.get(o)+1);});
             Map<SegmentedObject, Set<SegmentedObject>> divMap = new HashMapGetCreate.HashMapGetCreateRedirected<>(o->new HashSet<>());
@@ -249,7 +252,7 @@ public class BacteriaEdmDisplacementCategories implements TrackerSegmenter, Test
                 toReMatch.forEach(p -> {
                     // match prev daughters and current daughters
                     // min distance when both groups are centered at same y coordinate
-                    Map<SegmentedObject, Set<SegmentedObject>> match = match(new ArrayList<>(p.key), new ArrayList<>(p.value));
+                    Map<SegmentedObject, Set<SegmentedObject>> match = rematch(new ArrayList<>(p.key), new ArrayList<>(p.value));
                     logger.debug("double division detected @ frame: {}, {}->{} : new match: {}", p.value.iterator().next().getFrame(), p.key, p.value, match.entrySet());
                     // update prevMap, divMap and prevCount
                     match.forEach((prev, ns) -> {
@@ -301,8 +304,8 @@ public class BacteriaEdmDisplacementCategories implements TrackerSegmenter, Test
             });
         }
     }
-    // TODO find a matching method that would also work for swiming bacteria -> trackmate matching ?
-    protected static Map<SegmentedObject, Set<SegmentedObject>> match(List<SegmentedObject> prev, List<SegmentedObject> next) {
+    // TODO : find a method that would also work for swimming bacteria
+    protected static Map<SegmentedObject, Set<SegmentedObject>> rematch(List<SegmentedObject> prev, List<SegmentedObject> next) {
         double yPrev = meanY(prev);
         double yNext = meanY(next);
         double[] yCoordPrev = prev.stream().mapToDouble(o->o.getBounds().yMean() - yPrev).toArray();
@@ -349,4 +352,104 @@ public class BacteriaEdmDisplacementCategories implements TrackerSegmenter, Test
         this.stores=  stores;
     }
 
+
+    // utils
+    static Pair<Image[], int[][]> getResampledRawImages(int objectClassIdx, List<SegmentedObject> parentTrack, int[] targetImageShape) {
+        Image[] in = parentTrack.stream().map(p -> p.getPreFilteredImage(objectClassIdx)).toArray(Image[]::new);
+        int[][] shapes = ResizeUtils.getShapes(in, false);
+        // also scale by min/max
+        MinMaxScaler scaler = new MinMaxScaler();
+        IntStream.range(0, in.length).parallel().forEach(i -> in[i] = scaler.scale(in[i])); // scale before resample so that image is converted to float
+        Image[] inResampled = ResizeUtils.resample(in, in, false, new int[][]{targetImageShape});
+        return new Pair<>(inResampled, shapes);
+    }
+    static Image[][] getInputs(Image[] images, boolean addPrev, boolean addNext) {
+        if (!addPrev && !addNext) {
+            return IntStream.range(0, images.length).mapToObj(i -> new Image[]{images[i]}).toArray(Image[][]::new);
+        } else if (addPrev && addNext) {
+            return IntStream.range(0, images.length).mapToObj(i -> new Image[]{i==0 ? images[0] : images[i-1], images[i], i==images.length-1 ? images[i] : images[i+1]}).toArray(Image[][]::new);
+        } else if (addPrev && !addNext) {
+            return IntStream.range(0, images.length).mapToObj(i -> i==0 ? new Image[]{images[0], images[0]} : new Image[]{images[i-1], images[i]}).toArray(Image[][]::new);
+        } else {
+            return IntStream.range(0, images.length).mapToObj(i -> i==images.length-1 ? new Image[]{images[i], images[i]} : new Image[]{images[i], images[i+1]}).toArray(Image[][]::new);
+        }
+    }
+    static SegmentedObject getClosest(SegmentedObject source, List<SegmentedObject> targets, Map<SegmentedObject, TrackingObject> objectSpotMap) {
+        TrackingObject sourceTo = objectSpotMap.get(source);
+        double min = Double.POSITIVE_INFINITY;
+        SegmentedObject minO = null;
+        for (SegmentedObject target : targets) {
+            double dist =  objectSpotMap.get(target).squareDistanceTo(sourceTo);
+            if (dist < min) {
+                min = dist;
+                minO = target;
+            }
+        }
+        if (Double.isFinite(min)) return minO;
+        else return null;
+    }
+
+    static void removeCrossingLinks(Map<Integer, List<SegmentedObject>> objectsF, Map<SegmentedObject, TrackingObject> objectSpotMap) {
+        // ensure no crossing // TODO for open channel start with middle object
+        int minFrame = objectsF.keySet().stream().mapToInt(i->i).min().getAsInt();
+        int maxFrame = objectsF.keySet().stream().mapToInt(i->i).max().getAsInt();
+        for (int frame = minFrame; frame<=maxFrame; ++frame) {
+            if (!objectsF.containsKey(frame)) continue;
+            List<SegmentedObject> regions = objectsF.get(frame).stream().sorted(Comparator.comparingDouble(r->r.getBounds().yMean())).collect(Collectors.toList());
+            for (int idx = 1; idx<regions.size(); ++idx) {
+                TrackingObject up = objectSpotMap.get(regions.get(idx-1));
+                TrackingObject down = objectSpotMap.get(regions.get(idx));
+                int curMin=down.r.getBounds().yMin() + down.curToPrev.yMin();
+                int prevMax = up.r.getBounds().yMax() + up.curToPrev.yMin();
+                if (curMin<prevMax) {
+                    down.curToPrev.translate(new SimpleOffset(0, prevMax-curMin, 0));
+                }
+            }
+        }
+    }
+    static class TrackingObject{ //extends fiji.plugin.trackmate.Spot {
+        final Region r;
+        final BoundingBox cur, curToPrev;
+        final double size;
+        final int frame;
+        final double dy;
+        public TrackingObject(Region r, Offset offset, int frame, double dy) {
+            //super( r.getGeomCenter(false).translateRev(offset), 1, 1);
+            this.r=r;
+            Offset offRev = new SimpleOffset(offset).reverseOffset();
+            this.cur = new SimpleBoundingBox(r.getBounds()).translate(offRev);
+            this.curToPrev = new SimpleBoundingBox(cur).translate(new SimpleOffset(0, -(int)(dy+0.5), 0));
+            size = r.size();
+            this.frame = frame;
+            this.dy=dy;
+            //getFeatures().put("dy", dy);
+        }
+        public int frame() {
+            return frame;
+        }
+
+        //@Override
+        public double squareDistanceTo( final TrackingObject s ) {
+            if (s instanceof TrackingObject) {
+                TrackingObject  next  = (TrackingObject)s;
+                if (frame()==next.frame()+1) return next.squareDistanceTo(this);
+                if (frame()!=next.frame()-1) return Double.POSITIVE_INFINITY;
+
+                /*return Math.pow(getFeature(POSITION_X) - next.getFeature(POSITION_X),2) +
+                        Math.pow(getFeature(POSITION_Y) - (next.getFeature(POSITION_Y) - next.getFeature("dy")) , 2) + // translate next y coord
+                        Math.pow(getFeature(POSITION_Z) - next.getFeature(POSITION_Z),2);
+                */
+                // other possible distance: overlap of translated region -> would be more efficient with a regression of distance to split parent center when division, or when previous object is not divided when tracker thought it was
+                /*double overlap = r.getOverlapArea(next.r, cur, next.curToPrev);
+                if (overlap==0) return Double.POSITIVE_INFINITY;
+                return 1 - overlap / next.size;*/
+                // overlap on y axis only
+
+                double overlap = Math.max(0, Math.min(cur.yMax(), next.curToPrev.yMax()) - Math.max(cur.yMin(), next.curToPrev.yMin()));
+                if (overlap==0) return Double.POSITIVE_INFINITY;
+                return 1 - overlap / next.curToPrev.sizeY();
+
+            } else return Double.POSITIVE_INFINITY;
+        }
+    }
 }
