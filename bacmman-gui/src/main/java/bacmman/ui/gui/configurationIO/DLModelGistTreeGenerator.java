75d93ab3e1f639fdce878195c1d4e79d99d98035
jean.ollion <jean.ollion@polytechnique.org>
2020-03-10 18:32:51 +0100

    comment

--- bacmman-gui/src/main/java/bacmman/ui/gui/configurationIO/DLModelGistTreeGenerator.java
+++ bacmman-gui/src/main/java/bacmman/ui/gui/configurationIO/DLModelGistTreeGenerator.java
@@ -146,6 +146,7 @@ public class DLModelGistTreeGenerator {
                         } catch (final IOException | URISyntaxException er) {
                             logger.info("Error while trying to access URL", er);
                         }
+                        // TODO: direct download. gdrive case -> something like: wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O FILENAME && rm -rf /tmp/cookies.txt
                     }
                 }
             }
