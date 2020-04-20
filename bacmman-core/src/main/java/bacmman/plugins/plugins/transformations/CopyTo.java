962f0fa9aac929e1ecc282cbd3785ecfd406b5aa
jean.ollion <jean.ollion@polytechnique.org>
2020-04-20 11:31:53 +0200

    CopyTo.java allows to select a few planes

--- bacmman-core/src/main/java/bacmman/plugins/plugins/transformations/CopyTo.java
+++ bacmman-core/src/main/java/bacmman/plugins/plugins/transformations/CopyTo.java
@@ -1,5 +1,8 @@
 package bacmman.plugins.plugins.transformations;
 
+import bacmman.configuration.parameters.ConditionalParameter;
+import bacmman.configuration.parameters.EnumChoiceParameter;
+import bacmman.configuration.parameters.IntervalParameter;
 import bacmman.configuration.parameters.Parameter;
 import bacmman.data_structure.input_image.InputImages;
 import bacmman.image.Image;
@@ -9,9 +12,15 @@ import bacmman.plugins.MultichannelTransformation;
 
 import java.util.Arrays;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class CopyTo implements ConfigurableTransformation, MultichannelTransformation, Hint {
     Image[] source;
+    enum COPY_ZPLANES {COPY_ALL, MIDDLE_PLANE, TOP, BOTTOM, INTERVAL};
+    EnumChoiceParameter<COPY_ZPLANES> copyPlanes = new EnumChoiceParameter<>("Copy all Z- planes", COPY_ZPLANES.values(), COPY_ZPLANES.COPY_ALL).setHint("If the channel has several planes, this option allows to copy only one plane to another channel");
+    IntervalParameter planeInterval = new IntervalParameter("Z-plane interval", 0, 0, null, 0, 1).setHint("Plane interval to copy: from lower (included) to upper (excluded)");
+    ConditionalParameter<COPY_ZPLANES> copyPlanesCond = new ConditionalParameter<>(copyPlanes).setActionParameters(COPY_ZPLANES.INTERVAL, planeInterval);
+
     @Override
     public String getHintText() {
         return "This transformation simply copies the content of a channel to other channels. This is useful when using duplicated channels, in order to avoid computing several times the same transformations";
@@ -29,13 +38,37 @@ public class CopyTo implements ConfigurableTransformation, MultichannelTransform
 
     @Override
     public Parameter[] getParameters() {
-        return new Parameter[0];
+        return new Parameter[]{copyPlanesCond};
     }
 
     @Override
     public void computeConfigurationData(int channelIdx, InputImages inputImages) {
         Image[] allImages = InputImages.getImageForChannel(inputImages, channelIdx, false);
-        source = Arrays.stream(allImages).map((Function<Image, Image>) Image::duplicate).toArray(Image[]::new);
+        Function<Image, Image> dup;
+        switch (copyPlanes.getSelectedEnum()) {
+            case COPY_ALL:
+            default: {
+                dup = Image::duplicate;
+                break;
+            } case TOP: {
+                dup = im -> im.getZPlane(im.sizeZ()-1).duplicate();
+                break;
+            } case BOTTOM: {
+                dup = im -> im.getZPlane(0).duplicate();
+                break;
+            } case MIDDLE_PLANE: {
+                dup = im -> im.getZPlane(im.sizeZ()/2).duplicate();
+                break;
+            } case INTERVAL: {
+                int[] interval = planeInterval.getValuesAsInt();
+                dup = im -> {
+                    Image[] ims = IntStream.range(interval[0], interval[1]).mapToObj(im::getZPlane).toArray(Image[]::new);
+                    return Image.mergeZPlanes(ims);
+                };
+                break;
+            }
+        }
+        source = Arrays.stream(allImages).map(dup).toArray(Image[]::new);
     }
 
     @Override
