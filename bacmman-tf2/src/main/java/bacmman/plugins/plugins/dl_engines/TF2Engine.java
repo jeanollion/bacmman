1987edd9cc8308312c0d638a958502cc5c018f19
jeanollion <smogollion@yahoo.fr>
2021-03-14 21:59:43 +0100

    init commit tf2

---
+++ bacmman-tf2/src/main/java/bacmman/plugins/plugins/dl_engines/TF2Engine.java
@@ -0,0 +1,8 @@
+package bacmman.plugins.plugins.dl_engines;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TF2Engine {
+    public final static Logger logger = LoggerFactory.getLogger(TF2Engine.class);
+}
---
+++ bacmman-tf2/src/main/java/bacmman/tf2/TensorWrapper.java
@@ -0,0 +1,13 @@
+package bacmman.tf2;
+
+import bacmman.image.Image;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tensorflow.Tensor;
+
+public class TensorWrapper {
+    public final static Logger logger = LoggerFactory.getLogger(TensorWrapper.class);
+//    public static Tensor<Float> fromImagesNC(Image[][] imageNC, int fromIncl, int toExcl, float[][] bufferContainer, boolean... flipXYZ) {
+//    public static Image[][] getImagesNC(Tensor<Float> tensor, float[][] bufferContainer, boolean... flipXYZ) {
+//    public static void addToImagesNC(Image[][] targetNC, Tensor<Float> tensor, float[][] bufferContainer, boolean... flipXYZ) {
+}
