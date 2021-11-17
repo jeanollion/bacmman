9077b478a69afbd7f7da06e7e0c51d478fc27b6a
jeanollion <smogollion@yahoo.fr>
2021-11-17 15:25:50 +0100

    major refactoring (no retro-compatibility): added foreground selection method with thresholds on seed & propagation. Modified localExtremaRadius parameter

--- bacmman-core/src/main/java/bacmman/plugins/plugins/segmenters/WatershedSegmenter.java
+++ bacmman-core/src/main/java/bacmman/plugins/plugins/segmenters/WatershedSegmenter.java
@@ -18,64 +18,104 @@
  */
 package bacmman.plugins.plugins.segmenters;
 
+import bacmman.configuration.parameters.*;
 import bacmman.core.Core;
 import bacmman.data_structure.RegionPopulation;
 import bacmman.data_structure.SegmentedObject;
+import bacmman.image.BoundingBox;
 import bacmman.image.Image;
 import bacmman.image.ImageByte;
+import bacmman.image.ImageMask;
+import bacmman.plugins.TestableProcessingPlugin;
+import bacmman.plugins.Thresholder;
+import bacmman.plugins.plugins.thresholders.Percentile;
 import bacmman.processing.Filters;
 import bacmman.processing.watershed.WatershedTransform;
 import bacmman.plugins.Segmenter;
 import bacmman.plugins.plugins.thresholders.IJAutoThresholder;
-import bacmman.configuration.parameters.BooleanParameter;
-import bacmman.configuration.parameters.BoundedNumberParameter;
-import bacmman.configuration.parameters.NumberParameter;
-import bacmman.configuration.parameters.Parameter;
-import bacmman.configuration.parameters.PluginParameter;
-import bacmman.configuration.parameters.PreFilterSequence;
 import ij.process.AutoThresholder;
 import bacmman.plugins.SimpleThresholder;
 import bacmman.plugins.plugins.pre_filters.ImageFeature;
+
+import java.util.Map;
 
 /**
  *
  * @author Jean Ollion
  */
-public class WatershedSegmenter implements Segmenter {
-    PreFilterSequence watershedMapFilters = new PreFilterSequence("Watershed Map").add(new ImageFeature().setFeature(ImageFeature.Feature.GRAD)).setHint("Filter sequence to compute the map on wich the watershed will be performed");
-    BooleanParameter decreasePropagation = new BooleanParameter("Decreasing propagation", false).setHint("On watershed map, whether propagation is done from local minima towards increasing insensity or from local maxima towards decreasing intensities");
-    NumberParameter localExtremaRadius = new BoundedNumberParameter("Local Extrema Radius", 2, 1.5, 1, null).setHint("Radius for local extrema computation in pixels");
-    PreFilterSequence intensityFilter = new PreFilterSequence("Intensity Filter").setHint("Fitler sequence to compute intensity map used to select forground regions");
-    PluginParameter<SimpleThresholder> threshlod = new PluginParameter<>("Threshold for foreground selection", bacmman.plugins.SimpleThresholder.class, new IJAutoThresholder().setMethod(AutoThresholder.Method.Otsu),false);
-    BooleanParameter foregroundOverThreshold = new BooleanParameter("Foreground is over threhsold", true);
-    public static boolean debug;
+public class WatershedSegmenter implements Segmenter, TestableProcessingPlugin {
+    PreFilterSequence watershedMapFilters = new PreFilterSequence("Watershed Map").add(new ImageFeature().setFeature(ImageFeature.Feature.HessianMax).setScale(2, 2)).setEmphasized(true).setHint("Filter sequence to compute the map on which the watershed will be performed");
+    BooleanParameter decreasePropagation = new BooleanParameter("Decreasing propagation", false).setEmphasized(true).setHint("Whether propagation is done from local minima towards increasing intensity or from local maxima towards decreasing intensities (local extrema and propagation are performed on watershed map)");
+    ScaleXYZParameter localExtremaRadius = new ScaleXYZParameter("Local Extrema Radius", 2, 2, false).setEmphasized(true);
+    enum FOREGROUND_SELECTION_METHOD {REGION_INTENSITY, PROPAGATION_THRESHOLD}
+    EnumChoiceParameter<FOREGROUND_SELECTION_METHOD> foregroundSelMethod = new EnumChoiceParameter<>("Foreground Selection Method", FOREGROUND_SELECTION_METHOD.values(), FOREGROUND_SELECTION_METHOD.REGION_INTENSITY).setEmphasized(true);
+
+    PreFilterSequence intensityFilter = new PreFilterSequence("Intensity Filter").setEmphasized(true).setHint("Filter sequence to compute intensity map used to select foreground regions");
+    PluginParameter<Thresholder> threshlod = new PluginParameter<>("Threshold for foreground selection", bacmman.plugins.Thresholder.class, new IJAutoThresholder().setMethod(AutoThresholder.Method.Otsu),false).setEmphasized(true);
+    BooleanParameter foregroundOverThreshold = new BooleanParameter("Foreground is over threshold", true).setEmphasized(true);
+
+    PluginParameter<Thresholder> seedThreshlod = new PluginParameter<>("Seed Threshold", bacmman.plugins.Thresholder.class, new Percentile().setPercentile(0.75),false).setEmphasized(true).setHint("Threshold computed on the watershed map to select seeds");
+    PluginParameter<Thresholder> propagationThreshlod = new PluginParameter<>("Propagation Threshold", bacmman.plugins.Thresholder.class, new Percentile().setPercentile(99),false).setEmphasized(true).setHint("Threshold computed on the input image / or on the watershed map (depending on the parameter <em>Propagation End Map</em>) to stop propagation");
+    BooleanParameter propagationThresholdOnInputImage = new BooleanParameter("Propagation End Map", "Input Image", "Watershed Map", true).setEmphasized(true);
+
+    ConditionalParameter<FOREGROUND_SELECTION_METHOD> foregroundSelMethodCond = new ConditionalParameter<>(foregroundSelMethod)
+            .setActionParameters(FOREGROUND_SELECTION_METHOD.REGION_INTENSITY, intensityFilter, threshlod, foregroundOverThreshold)
+            .setActionParameters(FOREGROUND_SELECTION_METHOD.PROPAGATION_THRESHOLD, seedThreshlod, propagationThreshlod, propagationThresholdOnInputImage);
     @Override
     public RegionPopulation runSegmenter(Image input, int objectClassIdx, SegmentedObject parent) {
         // perform watershed on all local extrema
         Image watershedMap = watershedMapFilters.filter(input, parent.getMask());
-        
-        ImageByte localExtrema = Filters.localExtrema(watershedMap, null, decreasePropagation.getSelected(), parent.getMask(), Filters.getNeighborhood(localExtremaRadius.getValue().doubleValue(), watershedMap));
+        double radXY = localExtremaRadius.getScaleXY();
+        double radZ = localExtremaRadius.getScaleZ(watershedMap.getScaleXY(), watershedMap.getScaleZ());
+        ImageByte localExtrema = Filters.localExtrema(watershedMap, null, decreasePropagation.getSelected(), parent.getMask(), Filters.getNeighborhood(radXY, radZ, watershedMap));
+        FOREGROUND_SELECTION_METHOD method = foregroundSelMethod.getSelectedEnum();
         WatershedTransform.WatershedConfiguration config = new WatershedTransform.WatershedConfiguration().decreasingPropagation(decreasePropagation.getSelected());
-        RegionPopulation pop = WatershedTransform.watershed(watershedMap, parent.getMask(), localExtrema, config);
-        // remove regions with low intensity value
-        Image intensityMap = intensityFilter.filter(input, parent.getMask());
-        if (debug) {
-            Core.showImage(input.duplicate("input map"));
-            Core.showImage(localExtrema.duplicate("local extrema"));
-            Core.showImage(watershedMap.duplicate("watershed map"));
-            Core.showImage(intensityMap.duplicate("intensity map"));
+        if (method.equals(FOREGROUND_SELECTION_METHOD.PROPAGATION_THRESHOLD)) {
+            // erase seed depending on threshold
+            double seedThld = seedThreshlod.instantiatePlugin().runThresholder(watershedMap, parent);
+            BoundingBox.LoopPredicate lp = decreasePropagation.getSelected() ? (x, y, z) -> watershedMap.getPixel(x, y, z)<seedThld : (x, y, z) -> watershedMap.getPixel(x, y, z)>seedThld;
+            ImageMask.loop(localExtrema, (x, y, z)->localExtrema.setPixel(x, y, z, 0), lp);
+            // set propagation criterion
+            Image propEndMap = propagationThresholdOnInputImage.getSelected() ? input : watershedMap;
+            double propThld = propagationThreshlod.instantiatePlugin().runThresholder(propEndMap, parent);
+            config.propagationCriterion(new WatershedTransform.ThresholdPropagation(propEndMap, propThld, propagationThresholdOnInputImage.getSelected() || decreasePropagation.getSelected()));
+            if (stores!=null) {
+                Core.userLog("WatershedSegmenter: seed threshold: "+seedThld+ " propagation Thld: "+propThld);
+            }
+
         }
-        double thld = threshlod.instantiatePlugin().runSimpleThresholder(intensityMap, parent.getMask());
-        
-        int tot = pop.getRegions().size();
-        pop.filter(new RegionPopulation.MeanIntensity(thld, foregroundOverThreshold.getSelected(), intensityMap));
-        if (debug) logger.debug("WatershedSegmenter: threshold: {}, kept: {}/{}", thld, pop.getRegions().size(), tot);
+
+        RegionPopulation pop = WatershedTransform.watershed(watershedMap, parent.getMask(), localExtrema, config);
+
+
+        if (stores!=null) {
+            if (stores.get(parent).isExpertMode()) stores.get(parent).addIntermediateImage("local extrema", localExtrema);
+            stores.get(parent).addIntermediateImage("watershed map", watershedMap);
+
+        }
+        if (method.equals(FOREGROUND_SELECTION_METHOD.REGION_INTENSITY)) { // remove regions with low intensity value
+            Image intensityMap = intensityFilter.filter(input, parent.getMask());
+            double thld = threshlod.instantiatePlugin().runThresholder(intensityMap, parent);
+            int tot = pop.getRegions().size();
+            pop.filter(new RegionPopulation.MeanIntensity(thld, foregroundOverThreshold.getSelected(), intensityMap));
+            if (stores!=null) {
+                stores.get(parent).addIntermediateImage("intensity Map", intensityMap);
+                logger.debug("WatershedSegmenter: threshold: {}, kept: {}/{}", thld, pop.getRegions().size(), tot);
+                Core.userLog("WatershedSegmenter: threshold: "+thld);
+            }
+        }
+
+
         return pop;
     }
     
     @Override
     public Parameter[] getParameters() {
-        return new Parameter[]{watershedMapFilters, decreasePropagation, localExtremaRadius, intensityFilter, threshlod, foregroundOverThreshold};
+        return new Parameter[]{watershedMapFilters, decreasePropagation, localExtremaRadius, foregroundSelMethodCond};
     }
-    
+    Map<SegmentedObject, TestDataStore> stores;
+    @Override
+    public void setTestDataStore(Map<SegmentedObject, TestDataStore> stores) {
+        this.stores=stores;
+    }
 }
