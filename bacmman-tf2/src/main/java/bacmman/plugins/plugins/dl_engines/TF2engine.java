cba10476915a98c09520c0c87e1ca85f62aadd1f
jeanollion <smogollion@yahoo.fr>
2022-01-14 20:11:25 +0100

    - configure from metadata -> added Z-Axis

--- bacmman-tf2/src/main/java/bacmman/plugins/plugins/dl_engines/TF2engine.java
+++ bacmman-tf2/src/main/java/bacmman/plugins/plugins/dl_engines/TF2engine.java
@@ -28,17 +28,19 @@ public class TF2engine implements DLengine, Hint, DLMetadataConfigurable {
 
     @Override
     public void configureFromMetadata(DLModelMetadata metadata) {
-        if (!metadata.getInputs().get(0).is3D()) zAxis.setValue(Z_AXIS.BATCH);
-        // TODO add supp parameter to consider Z axis as channel for Talissman
-        // TODO what if severl inputs ?
+        if (!metadata.getInputs().get(0).is3D()) zAxis.setValue(Z_AXIS.BATCH); // TODO what if several inputs ?
+        TextParameter zAxis = metadata.getOtherParameter("Z-Axis", TextParameter.class);
+        if (zAxis!=null) {
+            logger.debug("Z-axis configured from metadata: {}", zAxis.getValue());
+            this.zAxis.setSelectedItem(zAxis.getValue());
+        } else logger.debug("no Z-Axis in metadata");
     }
 
     enum Z_AXIS {Z, CHANNEL, BATCH}
-    //FileChooser modelFile = new FileChooser("Tensorflow model", FileChooser.FileChooserOption.DIRECTORIES_ONLY, false).setEmphasized(true).setHint("Select the folder containing the saved model (.pb file)");
     DLModelFileParameter modelFile = new DLModelFileParameter("Tensorflow model").setEmphasized(true).setHint("Select the folder containing the saved model (.pb file)");
     BoundedNumberParameter batchSize = new BoundedNumberParameter("Batch Size", 0, 16, 0, null).setEmphasized(true).setHint("Size of the mini batches. Reduce to limit out-of-memory errors, and optimize according to the device");
     ArrayNumberParameter flip = InputShapesParameter.getInputShapeParameter(false, true, new int[]{0, 0}, 1).setName("Average Flipped predictions").setHint("If 1 is set to an axis, flipped image will be predicted and averaged with original image. If 1 is set to X and Y axis, 3 flips are performed (X, Y and XY) which results in a 4-fold prediction number");
-    EnumChoiceParameter<Z_AXIS> zAxis = new EnumChoiceParameter<>("Z axis", Z_AXIS.values(), Z_AXIS.Z).setHint("Choose how to handle Z axis: <ul><li>Z_AXIS: treated as 3rd space dimension.</li><li>CHANNEL: Z axis will be considered as channel axis. In case the tensor has several channels, the channel defined in <em>Channel Index</em> parameter will be used</li><li>BATCH: tensor are treated as 2D images </li></ul>");
+    EnumChoiceParameter<Z_AXIS> zAxis = new EnumChoiceParameter<>("Z-Axis", Z_AXIS.values(), Z_AXIS.Z).setHint("Choose how to handle Z axis: <ul><li>Z_AXIS: treated as 3rd space dimension.</li><li>CHANNEL: Z axis will be considered as channel axis. In case the tensor has several channels, the channel defined in <em>Channel Index</em> parameter will be used</li><li>BATCH: tensor are treated as 2D images </li></ul>");
     BoundedNumberParameter channelIdx = new BoundedNumberParameter("Channel Index", 0, 0, 0, null).setHint("Channel Used when Z axis is transposed to channel axis");
     ConditionalParameter<Z_AXIS> zAxisCond = new ConditionalParameter<>(zAxis)
             .setActionParameters(Z_AXIS.CHANNEL, channelIdx)
