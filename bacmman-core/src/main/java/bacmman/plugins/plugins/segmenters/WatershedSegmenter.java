1c970b07f2ad3327a5e261179a489bee5322cb2b
jeanollion <smogollion@yahoo.fr>
2022-06-07 11:13:38 +0200

    watershed segmenter implements objectSplitter interface

--- bacmman-core/src/main/java/bacmman/plugins/plugins/segmenters/WatershedSegmenter.java
+++ bacmman-core/src/main/java/bacmman/plugins/plugins/segmenters/WatershedSegmenter.java
@@ -20,28 +20,33 @@ package bacmman.plugins.plugins.segmenters;
 
 import bacmman.configuration.parameters.*;
 import bacmman.core.Core;
+import bacmman.data_structure.Region;
 import bacmman.data_structure.RegionPopulation;
 import bacmman.data_structure.SegmentedObject;
 import bacmman.image.*;
-import bacmman.plugins.TestableProcessingPlugin;
-import bacmman.plugins.Thresholder;
+import bacmman.plugins.*;
 import bacmman.plugins.plugins.thresholders.Percentile;
+import bacmman.plugins.plugins.trackers.ObjectIdxTracker;
 import bacmman.processing.Filters;
 import bacmman.processing.ImageFeatures;
+import bacmman.processing.clustering.RegionCluster;
+import bacmman.processing.split_merge.SplitAndMerge;
 import bacmman.processing.split_merge.SplitAndMergeEdge;
 import bacmman.processing.watershed.WatershedTransform;
-import bacmman.plugins.Segmenter;
 import bacmman.plugins.plugins.thresholders.IJAutoThresholder;
 import ij.process.AutoThresholder;
 import bacmman.plugins.plugins.pre_filters.ImageFeature;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  *
  * @author Jean Ollion
  */
-public class WatershedSegmenter implements Segmenter, TestableProcessingPlugin {
+public class WatershedSegmenter implements Segmenter, SegmenterSplitAndMerge, ObjectSplitter, TestableProcessingPlugin {
     PreFilterSequence watershedMapFilters = new PreFilterSequence("Watershed Map").add(new ImageFeature().setFeature(ImageFeature.Feature.HessianMax).setScale(2, 2)).setEmphasized(true).setHint("Filter sequence to compute the map on which the watershed will be performed");
     BooleanParameter decreasePropagation = new BooleanParameter("Decreasing propagation", false).setEmphasized(true).setHint("Whether propagation is done from local minima towards increasing intensity or from local maxima towards decreasing intensities (local extrema and propagation are performed on watershed map)");
     ScaleXYZParameter localExtremaRadius = new ScaleXYZParameter("Local Extrema Radius", 2, 2, false).setEmphasized(true);
@@ -68,11 +73,15 @@ public class WatershedSegmenter implements Segmenter, TestableProcessingPlugin {
     ConditionalParameter<Boolean> hessEdgesCond = new ConditionalParameter<>(computeHessian).setActionParameters(true, hessianEdges);
     ConditionalParameter<Boolean> mergeCond = new ConditionalParameter<>(mergeConnectedRegions).setActionParameters(true, mergeThreshold, normalizeEdgeValues, hessEdgesCond);
 
+    // store here images for use in split  / compute merge cost methods
+    Map<SegmentedObject, Image> edgeMap = new HashMap<>();
+    Map<SegmentedObject, Image> watershedMap = new HashMap<>();
 
     @Override
     public RegionPopulation runSegmenter(Image input, int objectClassIdx, SegmentedObject parent) {
         // perform watershed on all local extrema
         Image watershedMap = watershedMapFilters.filter(input, parent.getMask());
+        this.watershedMap.put(parent, watershedMap);
         double radXY = localExtremaRadius.getScaleXY();
         double radZ = localExtremaRadius.getScaleZ(watershedMap.getScaleXY(), watershedMap.getScaleZ());
         ImageByte localExtrema = Filters.localExtrema(watershedMap, null, decreasePropagation.getSelected(), parent.getMask(), Filters.getNeighborhood(radXY, radZ, watershedMap));
@@ -115,14 +124,16 @@ public class WatershedSegmenter implements Segmenter, TestableProcessingPlugin {
         }
         if (mergeConnectedRegions.getSelected()) {
             Image smMap =  computeHessian.getSelected() ? ImageFeatures.getHessian(input, hessianEdges.getScaleXY(), hessianEdges.getScaleZ(input.getScaleXY(), input.getScaleZ()), false)[0] : watershedMap;
+            this.edgeMap.put(parent, smMap);
+            if (stores!=null && computeHessian.getSelected()) stores.get(parent).addIntermediateImage("Edge Map", smMap);
             SplitAndMergeEdge sm = new SplitAndMergeEdge(smMap, input, mergeThreshold.getValue().doubleValue(), normalizeEdgeValues.getSelected());
-            if (stores!=null) stores.get(parent).addIntermediateImage("edge values", sm.drawInterfaceValues(pop));
+            if (stores!=null) stores.get(parent).addIntermediateImage("Edge Values", sm.drawInterfaceValues(pop));
             pop = sm.merge(pop, null);
         }
 
         return pop;
     }
-    
+
     @Override
     public Parameter[] getParameters() {
         return new Parameter[]{watershedMapFilters, decreasePropagation, localExtremaRadius, foregroundSelMethodCond, mergeCond};
@@ -131,5 +142,107 @@ public class WatershedSegmenter implements Segmenter, TestableProcessingPlugin {
     @Override
     public void setTestDataStore(Map<SegmentedObject, TestDataStore> stores) {
         this.stores=stores;
+    }
+
+    /// SegmenterSplitAndMerge
+    private Image getEdgeMap(Image input, SegmentedObject parent) {
+        if (edgeMap.containsKey(parent)) return edgeMap.get(parent);
+        if (computeHessian.getSelected()) {
+            Image edge = ImageFeatures.getHessian(input, hessianEdges.getScaleXY(), hessianEdges.getScaleZ(input.getScaleXY(), input.getScaleZ()), false)[0];
+            edgeMap.put(parent, edge);
+            return edge;
+        } else {
+            if (watershedMap.containsKey(parent)) return watershedMap.get(parent);
+            Image ws = watershedMapFilters.filter(input, parent.getMask());
+            edgeMap.put(parent, ws);
+            watershedMap.put(parent, ws);
+            return ws;
+        }
+    }
+
+    @Override
+    public double split(Image input, SegmentedObject parent, int structureIdx, Region o, List<Region> result) {
+        result.clear();
+        if (input==null) parent.getPreFilteredImage(structureIdx);
+        SplitAndMergeEdge sm = new SplitAndMergeEdge(getEdgeMap(input, parent), input, mergeThreshold.getValue().doubleValue(), normalizeEdgeValues.getSelected());
+
+        RegionPopulation pop =  splitObject(input, parent, structureIdx, o, sm); // after this step pop is in same landmark as o's landmark
+        if (pop.getRegions().size()<=1) return Double.POSITIVE_INFINITY;
+        else {
+            result.addAll(pop.getRegions());
+            if (pop.getRegions().size()>2) return 0; //   objects could not be merged during step process means no contact (effect of local threshold)
+            SplitAndMergeEdge.Interface inter = getInterface(result.get(0), result.get(1), sm, new ImageByte("split mask", parent.getMask()));
+            //logger.debug("split @ {}-{}, inter size: {} value: {}/{}", parent, o.getLabel(), inter.getVoxels().size(), inter.value, splitAndMerge.splitThresholdValue);
+            if (inter.getVoxels().size()<=1) return 0;
+            double cost = getCost(inter.getValue(), mergeThreshold.getValue().doubleValue(), true);
+            return cost;
+        }
+
+    }
+
+    private SplitAndMergeEdge.Interface getInterface(Region o1, Region o2, SplitAndMergeEdge sm, ImageByte tempSplitMask) {
+        o1.draw(tempSplitMask, o1.getLabel());
+        o2.draw(tempSplitMask, o2.getLabel());
+        SplitAndMergeEdge.Interface inter = RegionCluster.getInteface(o1, o2, tempSplitMask, sm.getFactory());
+        inter.updateInterface();
+        o1.draw(tempSplitMask, 0);
+        o2.draw(tempSplitMask, 0);
+        return inter;
+    }
+
+    public RegionPopulation splitObject(Image input, SegmentedObject parent, int structureIdx, Region object, SplitAndMerge sm) {
+        ImageInteger mask = object.isAbsoluteLandMark() ? object.getMaskAsImageInteger().cropWithOffset(input.getBoundingBox()) :object.getMaskAsImageInteger().cropWithOffset(input.getBoundingBox().resetOffset()); // extend mask to get the same size as the image
+        if (smVerbose && stores!=null) sm.setTestMode(TestableProcessingPlugin.getAddTestImageConsumer(stores, parent));
+        RegionPopulation res = sm.splitAndMerge(mask, 10, sm.objectNumberLimitCondition(2));
+        res.sortBySpatialOrder(ObjectIdxTracker.IndexingOrder.YXZ);
+        if (object.isAbsoluteLandMark()) res.translate(parent.getBounds(), true);
+        if (res.getRegions().size()>2) RegionCluster.mergeUntil(res, 2, 0); // merge most connected until 2 objects remain
+        return res;
+    }
+
+    @Override public double computeMergeCost(Image input, SegmentedObject parent, int structureIdx, List<Region> objects) {
+        if (objects.isEmpty() || objects.size()==1) return 0;
+        if (input==null) input = parent.getPreFilteredImage(structureIdx);
+        RegionPopulation mergePop = new RegionPopulation(objects, objects.get(0).isAbsoluteLandMark() ? input : new BlankMask(input).resetOffset());
+        mergePop.relabel(false); // ensure distinct labels , if not cluster cannot be found
+        SplitAndMergeEdge sm = new SplitAndMergeEdge(getEdgeMap(input, parent), input, mergeThreshold.getValue().doubleValue(), normalizeEdgeValues.getSelected());
+
+        RegionCluster<SplitAndMergeEdge.Interface> c = new RegionCluster<>(mergePop, true, sm.getFactory());
+        List<Set<Region>> clusters = c.getClusters();
+        if (clusters.size()>1) { // merge impossible : presence of disconnected objects
+            if (smVerbose && stores!=null) logger.debug("merge impossible: {} disconnected clusters detected", clusters.size());
+            return Double.POSITIVE_INFINITY;
+        }
+        double maxCost = Double.NEGATIVE_INFINITY ;
+        Set<SplitAndMergeEdge.Interface> allInterfaces = c.getInterfaces(clusters.get(0));
+        for (SplitAndMergeEdge.Interface i : allInterfaces) {
+            i.updateInterface();
+            if (i.getValue() > maxCost) maxCost = i.getValue();
+        }
+
+        if (Double.isInfinite(maxCost)) return Double.POSITIVE_INFINITY;
+        return getCost(maxCost, mergeThreshold.getValue().doubleValue(), false);
+
+    }
+    public static double getCost(double value, double threshold, boolean valueShouldBeBelowThresholdForAPositiveCost)  {
+        if (valueShouldBeBelowThresholdForAPositiveCost) {
+            if (value>=threshold) return 0;
+            else return (threshold-value);
+        } else {
+            if (value<=threshold) return 0;
+            else return (value-threshold);
+        }
+    }
+
+    // Object Splitter implementation
+    @Override
+    public RegionPopulation splitObject(Image input, SegmentedObject parent, int structureIdx, Region object) {
+        SplitAndMergeEdge sm = new SplitAndMergeEdge(getEdgeMap(input, parent), input, mergeThreshold.getValue().doubleValue(), normalizeEdgeValues.getSelected());
+        return splitObject(input, parent, structureIdx, object, sm );
+    }
+    boolean smVerbose;
+    @Override
+    public void setSplitVerboseMode(boolean verbose) {
+        smVerbose=verbose;
     }
 }
