899e427e933726526aa52e6d23717bf84ee840f6
jeanollion <smogollion@yahoo.fr>
2025-06-18 10:35:14 +0200

    ObjectFeatures : Intensity measurement : added measurement on central plane

--- bacmman-core/src/main/java/bacmman/plugins/plugins/measurements/ObjectFeatures.java
+++ bacmman-core/src/main/java/bacmman/plugins/plugins/measurements/ObjectFeatures.java
@@ -37,6 +37,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -53,8 +54,8 @@ public class ObjectFeatures implements Measurement, Hint {
             });;
     SimpleListParameter<PluginParameter<ObjectFeature>> features = new SimpleListParameter<>("Features", def).setMinChildCount(1).setChildrenNumber(1).setEmphasized(true);
     PreFilterSequence preFilters = new PreFilterSequence("Pre-Filters").setHint("All intensity measurements features will be computed on the image filtered by the operation defined in this parameter.");
-    enum MODE_3D {ALL_PLANES, SINGLE_PLANE}
-    EnumChoiceParameter<MODE_3D> mode3D = new EnumChoiceParameter<MODE_3D>("3D measurement", MODE_3D.values(), MODE_3D.ALL_PLANES).setHint("For intensity measurement only: In case of 3D measurement: choose ALL_PLANES to perform regular 3D measurement, or SINGLE_PLANE to limit measurement to a single user-defined plane");
+    enum MODE_3D {ALL_PLANES, SINGLE_PLANE, CENTER_PLANE}
+    EnumChoiceParameter<MODE_3D> mode3D = new EnumChoiceParameter<MODE_3D>("3D measurement", MODE_3D.values(), MODE_3D.ALL_PLANES).setHint("For intensity measurement only: In case of 3D measurement: choose <br/>ALL_PLANES to perform regular 3D measurement <br/>SINGLE_PLANE to limit measurement to a single user-defined plane. <br/>CENTER_PLANE: to perform measurement on the plane of the Z coordinate of the center (linear interpolation for non-integer coordinate)");
     BoundedNumberParameter plane = new BoundedNumberParameter("Plane", 0, -1, 0, null).setHint("Choose plane to perform measurement on (zero-based index of plane)");
     ConditionalParameter<MODE_3D> mode3Dcond = new ConditionalParameter<>(mode3D).setActionParameters(MODE_3D.SINGLE_PLANE, plane);
     Parameter[] parameters = new Parameter[]{structure, preFilters, features, mode3Dcond};
@@ -122,15 +123,48 @@ public class ObjectFeatures implements Measurement, Hint {
         Map<Image, IntensityMeasurementCore> cores = new ConcurrentHashMap<>();
         BiFunction<Image, ImageMask, Image> pf = (im, mask) -> preFilters.filter(im,mask);
         for (PluginParameter<ObjectFeature> ofp : features.getActivatedChildren()) {
-            ObjectFeature f = ofp.instantiatePlugin();
-            if (f!=null) {
-                if (f instanceof IntensityMeasurement && mode3D.getSelectedEnum().equals(MODE_3D.SINGLE_PLANE)) ((IntensityMeasurement)f).limitToZ(plane.getIntValue());
-                f.setUp(parent, structureIdx, parent.getChildRegionPopulation(structureIdx));
-                if (f instanceof ObjectFeatureWithCore) ((ObjectFeatureWithCore)f).setUpOrAddCore(cores, pf);
-                parent.getChildren(structureIdx).forEach(o-> {
-                    double m = f.performMeasurement(o.getRegion());
-                    o.getMeasurements().setValue(((TextParameter)ofp.getAdditionalParameters().get(0)).getValue(), m);
-                });
+            ObjectFeature feature = ofp.instantiatePlugin();
+            if (feature!=null) {
+                int[] zMinMax = new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE};
+                if (feature instanceof IntensityMeasurement && mode3D.getSelectedEnum().equals(MODE_3D.CENTER_PLANE)) {
+                    parent.getChildren(structureIdx).mapToDouble(c -> c.getRegion().getCenterOrGeomCenter().getWithDimCheck(2))
+                        .forEach(z -> {
+                            if (((int)z) < zMinMax[0]) zMinMax[0] = (int)z;
+                            if (((int)Math.ceil(z)) > zMinMax[1]) zMinMax[1] = (int)Math.ceil(z);
+                        });
+                }
+                if (feature instanceof IntensityMeasurement && mode3D.getSelectedEnum().equals(MODE_3D.CENTER_PLANE) && zMinMax[0]!=zMinMax[1]) { // compute for each plane and measure intensity
+                    //logger.debug("center intensity measurement: [{}; {}]", zMinMax[0], zMinMax[1]);
+                    List<SegmentedObject> children = parent.getChildren(structureIdx).collect(Collectors.toList());
+                    double[][] measurements = new double[zMinMax[1] - zMinMax[0] + 1][children.size()];
+                    ObjectFeature fz = feature;
+                    for (int z = zMinMax[0]; z<=zMinMax[1]; ++z) {
+                        ((IntensityMeasurement) fz).limitToZ(z);
+                        fz.setUp(parent, structureIdx, parent.getChildRegionPopulation(structureIdx));
+                        ((ObjectFeatureWithCore) fz).setUpOrAddCore(cores, pf);
+                        for (int i = 0; i<measurements[z].length; ++i) {
+                            if (bacmman.image.BoundingBox.containsZ(children.get(i).getBounds(), z)) measurements[z][i] = fz.performMeasurement(children.get(i).getRegion());
+                        }
+                        if (z<zMinMax[1]) fz = ofp.instantiatePlugin();
+                    }
+                    for (int i = 0; i<measurements[0].length; ++i) {
+                        double z = children.get(i).getRegion().getCenterOrGeomCenter().get(2);
+                        int z1 = (int)z;
+                        int z2 = (int)Math.ceil(z);
+                        double m = measurements[z1][i] * (z2 - z) + measurements[z2][i] * (z - z1);
+                        children.get(i).getMeasurements().setValue(((TextParameter) ofp.getAdditionalParameters().get(0)).getValue(), m);
+                    }
+                } else {
+                    if (feature instanceof IntensityMeasurement && mode3D.getSelectedEnum().equals(MODE_3D.SINGLE_PLANE))
+                        ((IntensityMeasurement) feature).limitToZ(plane.getIntValue());
+                    else if (feature instanceof IntensityMeasurement && mode3D.getSelectedEnum().equals(MODE_3D.CENTER_PLANE)) ((IntensityMeasurement) feature).limitToZ(zMinMax[0]);
+                    feature.setUp(parent, structureIdx, parent.getChildRegionPopulation(structureIdx));
+                    if (feature instanceof ObjectFeatureWithCore) ((ObjectFeatureWithCore) feature).setUpOrAddCore(cores, pf);
+                    parent.getChildren(structureIdx).forEach(o -> {
+                        double m = feature.performMeasurement(o.getRegion());
+                        o.getMeasurements().setValue(((TextParameter) ofp.getAdditionalParameters().get(0)).getValue(), m);
+                    });
+                }
             }
         }
     }
